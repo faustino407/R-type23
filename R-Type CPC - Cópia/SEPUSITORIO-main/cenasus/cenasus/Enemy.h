657a40a02501940c163f8bf9c26e465b9c9d168e
faustino407 <134146891+faustino407@users.noreply.github.com>
2023-05-20 20:52:36 +0100

    rtype

---
+++ R-Type CPC - Cópia/SEPUSITORIO-main/cenasus/cenasus/Enemy.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <allegro5/allegro.h>
+
+
+class Enemy
+{
+public:
+	Enemy(int star_);
+	void init();
+	void update();
+	void render();
+	void dispose();
+
+	float getX() { return x; }
+	float getY() { return y; }
+	float getRadius() { return radius; }
+	/*void hit() { lives--; }
+	bool isDead() { return lives <= 0; }*/
+
+	float toRadians(float angle) {
+		return angle * 3.1415 / 180;
+	}
+
+	void hit() { lives--; }
+	bool isDead() { return lives <= 0; }
+
+private:
+	int star;
+	float x;
+	float y;
+	float radius;
+
+	float speed;
+	float dx;
+	float dy;
+	float angle;
+
+	int lives;
+
+	//imagem
+	//ALLEGRO_BITMAP* bitmap;
+	
+	
+	ALLEGRO_COLOR color;
+	bool isOnScreen;
+};
+
---
+++ R-Type CPC - Cópia/SEPUSITORIO-main/cenasus/cenasus/Game.cpp
@@ -0,0 +1,79 @@
+﻿#include "Game.h"
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <algorithm>
+#include <functional>
+
+Game::Game() {}
+void Game::init() {
+	background = new Background("res/Fundo.png");
+	player.init();
+
+	for (int i = 0; i < 4; i++) {
+		enemies.emplace_back(new Enemy(1));
+		enemies.emplace_back(new Enemy(2));
+		
+	}
+}
+void Game::update() {
+	background->update();
+	player.update();
+
+	for (auto& enemy : enemies) {
+		enemy->update();
+	}
+
+	for (int i = 0; i < player.getBullets().size(); i++) {
+		Bullet* bullet = player.getBullets()[i];
+		float bx = bullet->getX();
+		float by = bullet->getY();
+		float br = bullet->getRadius();
+		for (int j = 0; j < enemies.size(); j++) {
+			Enemy* enemy = enemies[j];
+			float ex = enemy->getX();
+			float ey = enemy->getY();
+			float er = enemy->getRadius();
+			if (collide(bx, by, br, ex, ey, er)) {
+				// Decrease enemy's life
+				enemy->hit();
+				// Remove the bullet
+				bullet->kill();
+			}
+		}
+	}
+
+
+	// Remove dead enemies
+	for (int i = enemies.size() - 1; i >= 0; i--) {
+		Enemy* enemy = enemies[i];
+		if (enemy->isDead()) {
+			delete enemy;
+			enemies.erase(enemies.begin() + i);
+		}
+	}
+}
+
+
+void Game::handleInput() {
+	player.handleInput();
+}
+
+void Game::render() {
+	background->render();
+	player.render();
+
+	for (auto& enemy : enemies) {
+		enemy->render();
+	}
+}
+
+void Game::dispose() {
+	background->dispose();
+	player.dispose();
+
+	for (auto& enemy : enemies) {
+		enemy->dispose();
+		delete enemy;
+	}
+	enemies.clear();
+}
